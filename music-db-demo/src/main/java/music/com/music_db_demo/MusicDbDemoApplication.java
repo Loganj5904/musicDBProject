ee5b3df9e33107747dd1fd7384db3d35975b4674
loganj5904 <loganj5904@gmail.com>
2025-07-25 21:18:29 -0400

    first commit

---
+++ music-db-demo/src/main/java/music/com/music_db_demo/MusicDbDemoApplication.java
@@ -0,0 +1,13 @@
+package music.com.music_db_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicDbDemoApplication.class, args);
+	}
+
+}
---
+++ music-db-demo/src/test/java/music/com/music_db_demo/MusicDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package music.com.music_db_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
